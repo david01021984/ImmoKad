b3e0fb4a6036bc21ecbff84e47e6d63a3b41b67b
liaw <liaw200407@gmail.com>
2024-01-10 11:09:15 +0100

    recup des url

--- ImmoKad.py
+++ ImmoKad.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 
 i=1
 tabLinks = []
-while i <= 101:
+while i <= 5:
     lien = (f"https://www.immoweb.be/fr/recherche/maison-et-appartement/a-vendre?countries=BE&priceType=SALE_PRICE&page={i}")
     
     # URL du site web que vous souhaitez scraper
@@ -38,120 +38,10 @@ while i <= 101:
       
     time.sleep(3)
 clean_list=set(tabLinks)
-fichier = open("data.txt", "r")
-
-# for link in clean_list:
-#     print(link)
-
-# print(tabLinks)       
-
+chemin = "urls.txt"
+with open(chemin, 'w') as fichier:
+    # Écrire chaque élément de la liste dans une ligne du fichier
+    for element in clean_list:
+        fichier.write('"' + (element) + '"' + ","  +'\n')
 
 
-
-
-
-# Utiliser BeautifulSoup pour analyser le contenu HTML de la page
-# soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-# def scrapFor(word):
-#         if header.text == word:
-#             x = table.find("td",class_="classified-table__data")
-#             try :
-#                 print(f"{word} : {x.text.strip()}")
-#             except AttributeError:
-#                 print("Vide et none")
-
-# def scrapForSplit(word):
-#     if header.text == word:
-#             x = table.find("td",class_="classified-table__data")
-#             clean_x = (x.text.strip()).split(' ', 1)[0]                                                                          
-#             try:
-#                 print(f"{word} : {clean_x.strip()}")
-#             except AttributeError:
-#                 print("Vide et none")
-
-# # Trouver le tableau par sa classe
-# tables = soup.find_all('tr', class_='classified-table__row')
-# #print(tables)
-# for table in tables:
-#     #print(table)
-    
-#     header = table.find("th", class_="classified-table__header")
-#     # if header:
-#     #     print(header.text)
-#     #     data = table.find("td")
-#     #     print(data.text)
-
-#     if header :
-#         scrapForSplit("Surface du terrain")
-#         scrapForSplit("Surface du jardin")
-#         scrapForSplit("Surface habitable")
-#         scrapForSplit("Surface du salon")
-#         scrapForSplit("Surface de la cuisine")
-#         scrapForSplit("Surface du bureau")
-
-#         scrapFor("Chambres")        
-#         scrapFor("Classe énergétique")
-#         scrapFor("Toilettes")
-#         scrapFor("Salles de bains")
-#         scrapFor("Salles de douche")
-#         scrapFor("Nombre de façades")
-#         scrapFor("État du bâtiment")
-#         scrapFor("À partir de")
-#         scrapFor("Disponible le")
-#         scrapFor("Type de cuisine")
-#         scrapFor("Parkings intérieurs")
-#         scrapFor("Parkings extérieurs")
-#         scrapFor("Terrasse")
-#         scrapFor("Meublé")
-#         scrapFor("Bureau")
-
-
-
-# tables = soup.find_all('div', class_="classified__header-content")
-
-# for table in tables:
-#     #print(table)
-
-#     header = table.find("th", class_="classified-table__header")
-#     key = table.find("p",class_="classified__price")
-#     price = (key.text.strip()).split(' ', 1)[0]
-#     print(f"Le prix : {price.strip()}")
-
-#     address = table.find("span",class_="classified__information--address-row")
-    
-#     localite = table.find("span",{"aria-hidden":"true"})
-#     clean_loc = (address.text.strip()).split(' ',1)[0]
-#     print(f"Adresse : {clean_loc.strip()}")
-
-#     type_de_batiment = table.find("div",class_="classified__header-primary-info")
-#     clean_bat = (type_de_batiment.h1.text.strip()).split(' ',1)[0]
-#     print(f"Type de bien : {clean_bat.strip()}")
-
-
-# tables = soup.find_all('tr', class_='classified-table__row')
-
-
-# for page_number in range(1, 333):
-#     # Générer l'URL de la page actuelle
-#     if nbr_entree == 10000:
-#          break
-    
-#     page = url.format(page_number)
-    
-#     # Charger la page avec Selenium
-#     driver.get(page)
-    
-#     # Extraire les URLs et libellés des annonces de la page actuelle
-#     data = ()
-
-#     # Écrire les URLs et libellés dans le fichier CSV
-#     write_data_to_csv(data)
-#     # Appeler la fonction pour imprimer les URLs et libellés des annonces de la page actuelle
-#     print_urls_and_labels()
-    
-#     # Pause de 5 secondes avant de passer à la page suivante
-#     time.sleep(5)
-
-# # Fermer le navigateur après la récupération de toutes les entrées
-# driver.quit()
